8bd2c7519ae582a3b45157c9529003720b012671
vishalsingh0007 <vishalsingh071819@gmail.com>
2022-11-12 05:51:21 +0530

    day-4 completed

---
+++ product page/product.js
@@ -0,0 +1,353 @@
+var productArr = [
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000150_19-fresho-onion.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Onion",
+    kg: "1kg",
+    strike: "Rs60",
+    mrp: 38.38,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000144_13-fresho-ladies-finger.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Ladies Finger",
+    kg: "500g",
+    strike: "Rs23.19",
+    mrp: 19,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000074_19-fresho-cauliflower.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "CauliFlower",
+    kg: "1 pc (approx. 400 to 600 g)",
+    strike: "Rs23.19",
+    mrp: 19,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000283_11-fresho-green-peas.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Green Peas",
+    kg: "500g",
+    strike: "Rs42.50",
+    mrp: 34,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/40057966_2-fresho-tender-coconut-medium.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Tender Coconut - Medium",
+    kg: "1pc",
+    strike: "Rs38.75",
+    mrp: 31,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000103_16-fresho-cucumber.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Cucumber",
+    kg: "500g",
+    strike: "Rs17.15",
+    mrp: 14,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/40048457_7-fresho-potato-new-crop.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Potato - New Crop",
+    kg: "1kg",
+    strike: "Rs33.75",
+    mrp: 27,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000069_20-fresho-capsicum-green.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Capsicum - Green",
+    kg: "500g",
+    strike: "Rs52.50",
+    mrp: 42,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000263_11-fresho-strawberry.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Strawberry",
+    kg: "200g",
+    strike: "Rs60",
+    mrp: 48,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/40134281_11-fresho-baby-apple-shimla.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Baby Apple Shimla",
+    kg: "1 kg (Approx. 11-12 pcs)",
+    strike: "Rs215",
+    mrp: 172,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000066_25-fresho-cabbage.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Cabbage",
+    kg: "1 pc (approx. 500 g to 800 g)",
+    strike: "Rs17.50",
+    mrp: 12.12,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000127_17-fresho-lemon.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Lemon",
+    kg: "250g",
+    strike: "Rs22.50",
+    mrp: 18,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000269_30-fresho-pomegranate.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Pomegranate",
+    kg: "4 pcs (approx. 800 - 880 g)",
+    strike: "Rs281.25",
+    mrp: 161,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/s/10000054_17-fresho-brinjal-bottle-shape.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Brinjal - Bottle Shape",
+    kg: "500g",
+    strike: "Rs21.25",
+    mrp: 17,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/mm/40023474_7-fresho-banana-yelakki-organically-grown.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Banana - Yelakki, Organically Grown",
+    kg: "500g",
+    strike: "Rs50",
+    mrp: 40,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/mm/40004992_14-fresho-sweet-corn.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Sweet Corn",
+    kg: "2 pc",
+    strike: "Rs46",
+    mrp: 36,
+    brand: "Fresho"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/mm/1203204_2-bb-combo-gajar-halwa.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Gajar Halwa",
+    kg: "Combo 2 Items",
+    strike: "Rs154",
+    mrp: 128.55,
+    brand: "bb Combo"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/mm/1202745_1-bb-combo-pineapple-and-pomegranate-cut.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Pineapple and Pomegranate Cut",
+    kg: "Combo 2 Items",
+    strike: "Rs237.50",
+    mrp: 182,
+    brand: "bb Combo"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/mm/1203204_2-bb-combo-gajar-halwa.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Gajar Halwa",
+    kg: "Combo 2 Items",
+    strike: "Rs154",
+    mrp: 128.55,
+    brand: "bb Combo"
+  },
+  {
+    imageUrl:
+      "https://www.bigbasket.com/media/uploads/p/mm/1202745_1-bb-combo-pineapple-and-pomegranate-cut.jpg",
+    logo: "https://www.bbassets.com/static/v2514/custPage/build/content/img/vegicon.svg",
+    name: "Pineapple and Pomegranate Cut",
+    kg: "Combo 2 Items",
+    strike: "Rs237.50",
+    mrp: 182,
+    brand: "bb Combo"
+  },
+];
+
+displayproduct(productArr)
+var cartArr = JSON.parse(localStorage.getItem("cartitems")) || [];
+
+displayproduct(productArr)
+
+function displayproduct(productArr) {
+  document.querySelector("#innerbox").innerHTML = "";
+  productArr.map(function (elem, index) {
+    var div = document.createElement("div");
+    div.setAttribute("id", "my");
+    var p = document.createElement("p");
+    p.innerText = "GET 24% OFF";
+    var off = document.createElement("img");
+    off.setAttribute("src", "product1.png");
+
+    div.append(p, off);
+    var imgdiv = document.createElement("div");
+    imgdiv.setAttribute("id", "mainimgdiv");
+    var img = document.createElement("img");
+    img.setAttribute("src", elem.imageUrl);
+    imgdiv.append(img);
+
+    var div1 = document.createElement("div");
+    div1.setAttribute("id", "logo");
+
+    var logo = document.createElement("img");
+    logo.setAttribute("src", elem.logo);
+
+    var brand = document.createElement("p");
+    brand.innerText = elem.brand;
+
+    var name = document.createElement("p");
+    name.innerText = elem.name;
+    name.setAttribute("id", "name");
+
+    var kg = document.createElement("p");
+    kg.innerText = elem.kg;
+    kg.setAttribute("id", "kg");
+
+    var cart = document.createElement("div");
+    cart.setAttribute("id", "cart");
+
+    var pricediv = document.createElement("div");
+    pricediv.setAttribute("id", "price");
+
+    var p1 = document.createElement("p");
+    p1.innerText = "MRP:";
+    var p2 = document.createElement("p");
+    p2.innerText = elem.strike;
+    var p3 = document.createElement("p");
+    p3.innerText = "Rs";
+    p3.setAttribute("id", "mainmrp");
+    var p4 = document.createElement("p");
+    p4.innerText = elem.mrp;
+
+    pricediv.append(p1, p2, p3, p4);
+
+    var deli = document.createElement("div");
+    deli.setAttribute("id", "deli");
+
+    var truck = document.createElement("img");
+    truck.setAttribute(
+      "src",
+      "https://www.bbassets.com/static/v2514/custPage/build/content/img/standard-del-gray.svg"
+    );
+    var delitime = document.createElement("p");
+    delitime.innerText = "Standard Delivery: Tomorrow 3:00PM - 7:30PM";
+
+    var qtydiv = document.createElement("div");
+    qtydiv.setAttribute("id", "qtydiv");
+    var qty = document.createElement("p");
+    qty.innerText = "Qty";
+    qty.setAttribute("id", "qty");
+    var input = document.createElement("input");
+    input.value = 1
+    input.style.color = "#999"
+
+    var btn = document.createElement("button");
+    btn.innerText = "ADD";
+    btn.addEventListener("click", function () {
+      addtocart(elem)
+    })
+
+    qtydiv.append(qty, input, btn);
+    deli.append(truck, delitime);
+    cart.append(pricediv, deli, qtydiv);
+    var innerdiv = document.createElement("div");
+    div1.append(logo, brand, name);
+    innerdiv.append(div, imgdiv, div1, kg, cart);
+    document.querySelector("#innerbox").append(innerdiv);
+  });
+
+}
+
+
+// <------sorting-------->
+
+function pricesort() {
+  var pricesort = document.querySelector("#sorting").value
+  if (pricesort == "l2h") {
+    productArr.sort(function (a, b) {
+      return a.mrp - b.mrp;
+    })
+    displayproduct(productArr)
+  }
+  if (pricesort == "h2l") {
+    productArr.sort(function (a, b) {
+      return b.mrp - a.mrp;
+    })
+    displayproduct(productArr)
+  }
+}
+
+// <---------------------filter-------------->
+function filterfresho() {
+  var filterfresho = productArr.filter(function (elem) {
+    return elem.brand == "Fresho"
+  })
+  displayproduct(filterfresho)
+}
+
+function filterbb() {
+  var filterfresho = productArr.filter(function (elem) {
+    return elem.brand == "bb Combo"
+  })
+  displayproduct(filterfresho)
+}
+
+function search() {
+  var search = document.querySelector("#search").value
+  if (search != "") {
+    var filterfresho = productArr.filter(function (elem) {
+      return elem.brand == search
+    })
+    displayproduct(filterfresho)
+  }
+}
+displayproduct(productArr)
+
+// <-------------------addcart----------------->
+
+function addtocart(elem) {
+  elem.quantity = 1;
+  cartArr.push(elem)
+  localStorage.setItem("cartitems", JSON.stringify(cartArr))
+  document.querySelector("#cartnum").innerText = cartArr.length;
+}
+function cartpage() {
+  window.location.href = "../checkout page/checkout.html"
+}
